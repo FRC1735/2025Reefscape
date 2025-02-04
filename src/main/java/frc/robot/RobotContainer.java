0516fc75d9f1bbbea4f27e789246034f11ca6140
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2025-02-04 18:52:30 -0500

    YAGSL JSON Files

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,17 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.SwerveDriveSubsystem;
 
 public class RobotContainer {
+  // Subsystems
+  private final SwerveDriveSubsystem swerveDriveSubsystem = new SwerveDriveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve-2025"));
+
   public RobotContainer() {
     configureBindings();
   }
---
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Meter;
+
+import java.io.File;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+public class SwerveDriveSubsystem extends SubsystemBase {
+  private final boolean DEBUG = true;
+
+  private final SwerveDrive swerveDrive;
+  public double maximumSpeed = Units.feetToMeters(18.84);
+
+  public SwerveDriveSubsystem(File directory) {
+    SwerveDriveTelemetry.verbosity = DEBUG ? TelemetryVerbosity.HIGH : TelemetryVerbosity.NONE;
+
+    try {
+      swerveDrive = new SwerveParser(directory)
+                    .createSwerveDrive(maximumSpeed,
+                                        new Pose2d(new Translation2d(Meter.of(1),
+                                        Meter.of(4)),
+                                        Rotation2d.fromDegrees(0)));
+    } catch(Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    setupPathPlanner();
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  private void setupPathPlanner() {
+    // TODO
+  }
+}

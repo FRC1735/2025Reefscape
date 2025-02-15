261c21c677c6a6f487e6af88819b7637994190ea
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2025-02-15 09:24:21 -0500

    Clean up LockOnAprilTag command

--- src/main/java/frc/robot/commands/LockOnAprilTag.java
+++ src/main/java/frc/robot/commands/LockOnAprilTag.java
@@ -25,47 +25,34 @@ public class LockOnAprilTag extends Command {
 
   private static final boolean DEBUG = true;
 
-  /** Creates a new driveAimAtTarget. */
   public LockOnAprilTag(SwerveDriveSubsystem swerveDriveSubsystem, DoubleSupplier translationX, DoubleSupplier translationY, ControllerRumbleCallback controllerRumbleCallback) {
     this.swerve = swerveDriveSubsystem;
     this.translationX = translationX;
     this.translationY = translationY;
     lastGoodHeading = 0;
     this.controllerRumbleCallback = controllerRumbleCallback;
-    // Use addRequirements() here to declare subsystem dependencies.
+
     addRequirements(swerve);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // TODO - For now I only care about locking onto any april tag in view, could 
-    // TODO - add something like this to improve.
-    //swerve.setVisionTargetID(7);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     if(swerve.hasTarget()){
-      //heading = Math.pow(-SwerveSub.getVisionAngle()/30,3);
       heading = -swerve.getTargetXOffset() / 70;
-      
-      // TODO - would be cool to utilize haptic feedback to notify driver that this is working
-      //obotContainer.setRightRumbleDriver(0);
       controllerRumbleCallback.update(RumbleState.TARGET_LOCKED_ON);
     }else{
-      System.out.println("Warning: Swerve Aim: Lost Target!");
-      // TODO - would be cool to utilize haptic feedback to notify driver that this is working
-      //RobotContainer.setRightRumbleDriver(1);
+      if (DEBUG) {
+        System.out.println("Warning: Swerve Aim: Lost Target!");
+      }
       controllerRumbleCallback.update(RumbleState.TARGET_NONE);
       heading = 0;
     }
     lastGoodHeading = heading;
-
-    //System.out.println("Target is: " + heading);
-    //SwerveSub.driveCommand(translationX, translationY, heading);
-
+ 
     swerve.getSwerve().drive(new Translation2d(Math.pow(translationX.getAsDouble(), 3) * swerve.getSwerve().getMaximumChassisVelocity(),
     Math.pow(translationY.getAsDouble(), 3) * swerve.getSwerve().getMaximumChassisVelocity()),
     heading * swerve.getSwerve().getMaximumChassisAngularVelocity(),
@@ -82,16 +69,12 @@ false);
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    //RobotContainer.setRightRumbleDriver(0);
-    //System.out.println("Warning: Swerve Aim: Target Lost!");
-
     controllerRumbleCallback.update(RumbleState.TARGET_NONE);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    //return !SwerveSub.isValidVisionTarget();
     return false;
   }
 }

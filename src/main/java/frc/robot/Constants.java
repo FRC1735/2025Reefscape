ad36b8d4b08e64506fe9866cba81dcf016e3fbc3
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2025-02-18 09:30:12 -0500

    Real subsystem motor ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,25 +1,23 @@
 package frc.robot;
 
-    public final class Constants {
+public final class Constants {
     public static class CoralSubystemConstants {
-        public static final int LEAD_MOTOR_ID = 0;
-        public static final int FOLLOW_MOTOR_ID =0;
+        public static final int LEAD_MOTOR_ID = 4;
+        public static final int FOLLOW_MOTOR_ID = 5;
     }
 
     public static class AlgaeSubystemConstants {
-        public static final int MOTOR_ID = 0;
+        public static final int MOTOR_ID = 2;
 
     }
 
     public static class WristSubystemConstants {
-        public static final int MOTOR_ID = 0;
+        public static final int MOTOR_ID = 3;
 
     }
 
-
-
-public static class ElevatorSubystemConstants {
-    public static final int LEAD_MOTOR_ID = 0;
-    public static final int FOLLOW_MOTOR_ID =0;
+    public static class ElevatorSubystemConstants {
+        public static final int LEAD_MOTOR_ID = 6;
+        public static final int FOLLOW_MOTOR_ID = 7;
+    }   
 }
-    }

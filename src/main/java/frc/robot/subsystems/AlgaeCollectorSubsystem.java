eeb51dd7815f86235eb9ce2793312d32d412fb56
green-reaper-1735 <jared+1735@jaredandrews.com>
2025-03-13 23:44:16 -0400

    Update AlgaeCollectorSubsystem.java

--- src/main/java/frc/robot/subsystems/AlgaeCollectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeCollectorSubsystem.java
@@ -11,11 +11,13 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.SharpIR;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.AlgaeSubystemConstants;
+import frc.robot.utils.SmartDashboardPIDTuner;
 
 public class AlgaeCollectorSubsystem extends SubsystemBase {
   private SparkFlex motor = new SparkFlex(AlgaeSubystemConstants.MOTOR_ID, MotorType.kBrushless);
@@ -24,10 +26,17 @@ public class AlgaeCollectorSubsystem extends SubsystemBase {
 
   SharpIR distanceSensor = SharpIR.GP2Y0A41SK0F(0);
 
+  PIDController in3PIDController = new PIDController(1, 0, 0);
+
   public AlgaeCollectorSubsystem() {
     SparkFlexConfig motorConfig = new SparkFlexConfig();
     motorConfig.idleMode(IdleMode.kBrake);
     motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    in3PIDController.setTolerance(0.25);
+
+
+  
   }
 
   @Override
@@ -53,6 +62,23 @@ public class AlgaeCollectorSubsystem extends SubsystemBase {
     });
   }
 
+  public Command in2() {
+    return this.run(() -> {
+      if (algaePresent()) {
+        motor.set(-0.2);
+      } else {
+        motor.set(-1);
+      }
+    });
+  }
+
+  public Command in3() {
+    return this.run(() -> {
+      double result = in3PIDController.calculate(distanceSensor.getRangeCM(), 4);
+      // TODO - unclear to me if result should be negated
+      motor.set(result);
+    });
+  }
 
   public Command out() {
     return this.run(() -> motor.set(1));
